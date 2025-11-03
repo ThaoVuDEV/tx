0905e64210a2592ebf7dca140b433258b032acec
ThaoVu <thaovu.dev17@gmail.com>
2025-11-04 00:49:35 +0700

    u

--- config/database.js
+++ config/database.js
@@ -1,19 +1,7 @@
 
-//module.exports = {
-//	'url': 'mongodb://127.0.0.1:27017',
-//	'options': {
-//		'user':   'root',
-//		'pass':   '',
-//		'dbName': 'PlayGameChuan2',
-//		'useNewUrlParser': true,
-//		'useUnifiedTopology': true,
-//		//'autoIndex':       false,
-//	},
-//};
-
 
 module.exports = {
-	'url': 'mongodb+srv://thaotbja:Thaokentbja20@cluster0.3kowxkq.mongodb.net/?appName=Cluster0',
+	'url': 'mongodb+srv://thaotbja:Thaokentbja20@cluster0.3kowkq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 	'options': {
 	//	'user':   'root',
 	//	'pass':   '',
